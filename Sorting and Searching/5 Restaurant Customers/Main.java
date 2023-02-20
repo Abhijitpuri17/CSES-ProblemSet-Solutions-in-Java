371ebf37c721a38381719d80e1c6c3d1de05b449
Abhijit Puri <85777554+Abhijitpuri17@users.noreply.github.com>
2023-02-20 22:07:29 +0530

    Add: Solution of P5 Resturant Customers in Java

---
+++ Sorting and Searching/5 Restaurant Customers/Main.java
@@ -0,0 +1,78 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class Main {
+    static FastReader fr ;
+    static PrintWriter out ;
+    public static void main(String[] args) throws Exception{
+        fr = new FastReader() ;
+        out = new PrintWriter(System.out) ;
+        int n = fr.nextInt();
+        TreeMap<Integer, Integer> map = new TreeMap<>() ;
+        for(int i = 0; i < n; i++){
+            int a = fr.nextInt(), b = fr.nextInt();
+            map.put(a, map.getOrDefault(a, 0)+1);
+            map.put(b+1, map.getOrDefault(b+1, 0)-1) ;
+        }
+        long ans = 0 ;
+        long cur = 0 ;
+        for(int k: map.keySet()){
+            cur += map.get(k) ;
+            ans = Math.max(ans, cur) ;
+        }
+        out.print(ans);
+        out.close();
+    }
+    static class FastReader
+    {
+        final private int BUFFER_SIZE = 1 << 16;
+        private DataInputStream din;
+        private byte[] buffer;
+        private int bufferPointer, bytesRead;
+
+        public FastReader()
+        {
+            din = new DataInputStream(System.in);
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+        private void fillBuffer() throws IOException
+        {
+            bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+            if (bytesRead == -1)
+                buffer[0] = -1;
+        }
+
+        private byte read() throws IOException
+        {
+            if (bufferPointer == bytesRead)
+                fillBuffer();
+            return buffer[bufferPointer++];
+        }
+
+        public void close() throws IOException
+        {
+            if (din == null)
+                return;
+            din.close();
+        }
+        public int nextInt() throws IOException
+        {
+            int ret = 0;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+            do
+            {
+                ret = ret * 10 + c - '0';
+            }  while ((c = read()) >= '0' && c <= '9');
+
+            if (neg)
+                return -ret;
+            return ret;
+        }
+    }
+}
